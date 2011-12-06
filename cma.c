54c724125f022c0a3655d5a24149751bc3861db5
Ben <benkillinger@hotmail.com>
2011-12-05 22:07:45 -0800

    Added debug.h and modified cma.c to include debug macros

--- cma.c
+++ cma.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <limits.h>
 
+#include "debug.h"
 #include "cma.h"
 
 #define ITEMNOTFOUND ((void *)-1)
@@ -17,11 +18,13 @@ static struct {
 } class_counters={0,0,0,0,0,0};
 
 static MNode class_AddToList(MNode list, MNode item) {
+  ENTER;
   item->next = list;
-  return item;
+  RETURN(item);
 }
 
 static MNode class_RemoveFromList(MNode list,MNode item) {
+  ENTER;
   MNode p,prev;
 
   prev = NULL;
@@ -32,25 +35,32 @@ static MNode class_RemoveFromList(MNode list,MNode item) {
       else
 	prev->next = p->next;
       return list;
+      RETURN(list);
     }
     prev=p;
   }
   //not in list..
-  return ITEMNOTFOUND;
+  RETURN(ITEMNOTFOUND);
 }
 
 
 static void class_printList(MNode list) {
+  ENTER;
   if (!list)
+  {
+    EXIT;
     return;
+  }
   printf("Node %p, %ld\n",list,list->size);
   class_printList(list->next);
+  EXIT;
 }
 
 int class_memory(void *mem, size_t size) {
+  ENTER;
   MNode item;
   if (class_membase ) {
-    return FALSE;
+    RETURN(FALSE);
   }
 
   class_membase = mem;
@@ -60,50 +70,60 @@ int class_memory(void *mem, size_t size) {
   item = (MNode)mem;
   item->size=size-sizeof(struct MemNode);
   item->next = NULL;
-  
+  DEBUG("Creating Initial NoUseList with %x: %ud",item,size);
   class_nouse = class_AddToList(class_nouse,item);
+  EXIT;
 }
 
 void *class_calloc(size_t nmemb, size_t size) {
+  ENTER;
   void *mem;
 
   mem = class_malloc(nmemb*size);
   memset(mem,0,nmemb*size);
-  return mem;
+  RETURN(mem);
 }
 
 static MNode class_findNoUse(size_t target) {
+  ENTER;
   size_t closeness=LONG_MAX;
   size_t c;
   MNode best=NULL;
   MNode p;
 
+  DEBUG("Searching for a block of size: %ud",target);
   for (p=class_nouse;p!=NULL;p=p->next) {
     c = p->size - target;
     if (c >= 0 && c<closeness) {
       best = p;
       closeness=c;
+      DEBUG("Best is now: %x size=%ud",best,p->size);
     }
   }
-  return best;
+  RETURN(best);
 }
 
 MNode class_splitNode(MNode org,size_t size) {
+	ENTER;
 	MNode extra=NULL;
 	size_t orgsz = org->size;
 	
 	//we need room for a new header
 	if ( (orgsz-size-sizeof(struct MemNode)) > 0 ) {
+		DEBUG("Node split: %ud => %ud,%ud",org->size,size,orgsz-sizeof(struct MemNode)-size);
 		org->size = size;
 		extra = (MNode)((void*)org+size+sizeof(struct MemNode));
 		extra->next = 0;
 		extra->size = orgsz-sizeof(struct MemNode)-size;
 	}
+	else
+		DEBUG("Node does not have enough size to split:%ud %ud",org->size,size);
 	
-	return extra;
+	RETURN(extra);
 }
 
 void *class_malloc(size_t size) {
+  ENTER;
   MNode newnode,extra;
 
   newnode = class_findNoUse(size);
@@ -118,10 +138,10 @@ void *class_malloc(size_t size) {
     
     newnode->next = NULL;
     class_inuse = class_AddToList(class_inuse,newnode);
-    return (void *)newnode+sizeof(struct MemNode);
+    RETURN((void *)newnode+sizeof(struct MemNode));
   }
   else {
-    return NULL;
+    RETURN(NULL);
   }
 }
 
@@ -132,35 +152,43 @@ static void class_garbage() {
 }
 
 void class_free(void *ptr) {
+  ENTER;
   MNode cur=NULL;
   if (!ptr)
+  {
+    EXIT;
     return;
+  }
  
   cur=class_RemoveFromList(class_inuse,PTRTOMNODE(ptr));
   if (cur==ITEMNOTFOUND) {//not our pointer
+    EXIT;
     return;
   }
   class_inuse = cur;
   class_nouse = class_AddToList(class_nouse,PTRTOMNODE(ptr));
   class_garbage();
+  EXIT;
 }
 
 void *class_realloc(void *ptr, size_t size) {
+  ENTER;
   void *mem;
   size_t oldsize;
 
   mem=class_malloc(size);
   if (!mem)
-    return NULL;
+    RETURN(NULL);
 
   oldsize=PTRTOMNODE(ptr)->size;
   memcpy(mem,ptr,oldsize);
 
   class_free(ptr);
-  return mem;
+  RETURN(mem);
 }
 
 void class_stats() {
+  ENTER;
   printf("InUse\n");
   class_printList(class_inuse);
 
@@ -168,7 +196,7 @@ void class_stats() {
   class_printList(class_nouse);
 
   printf("Counters:\n");
-#define DUMPC(x) printf(" %10s : %d\n",#x,class_counters.x)
+#define DUMPC(x) printf(" %10s : %ld\n",#x,class_counters.x)
   DUMPC(malloc);
   DUMPC(calloc);
   DUMPC(realloc);
@@ -176,5 +204,6 @@ void class_stats() {
   DUMPC(gc);
   DUMPC(nomem);
 #undef DUMPC
+  EXIT;
 }
 
